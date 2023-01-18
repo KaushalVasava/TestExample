f16bd1844fa6fcca23077511046bf3d8b5169691
Kaushal-Vasava <kaushal.vasava@algozen.in>
2023-01-18 19:55:12 +0530

    Add password validation and reverse string validation test cases

--- app/src/main/java/com/lahsuak/apps/testexample/utils/Helper.kt
+++ app/src/main/java/com/lahsuak/apps/testexample/utils/Helper.kt
@@ -1,5 +1,7 @@
 package com.lahsuak.apps.testexample.utils
 
+import android.util.Log
+
 class Helper {
     fun isPalindrome(input: String): Boolean {
         var i = 0
@@ -14,5 +16,25 @@ class Helper {
             len--
         }
         return result
+    }
+
+    fun passwordValidation(input: String): String {
+        return if (input.isEmpty()) {
+            "String is empty"
+        } else if (input.length < 6) {
+            "length is less than 6"
+        } else if (input.length > 15) {
+            "length is more than 15"
+        } else {
+            "String is valid"
+        }
+    }
+
+    fun reverseString(input: String): String {
+        var reverseStr = ""
+        for (index in input.length - 1 downTo 0) {
+            reverseStr += input[index]
+        }
+        return reverseStr
     }
 }
--- app/src/test/java/com/lahsuak/apps/testexample/utils/HelperTest.kt
+++ app/src/test/java/com/lahsuak/apps/testexample/utils/HelperTest.kt
@@ -21,12 +21,51 @@ class HelperTest {
         println("After every test case")
     }
 
+//    @Test
+//    fun isPalindrome_inputString_level_expectedTrue() {
+//        //Arrange
+//        //Act
+//        val result = helper.isPalindrome("level")
+//        //Assert
+//        assertEquals(true, result)
+//    }
+
     @Test
-    fun isPalindrome_inputString_level_expectedTrue() {
-        //Arrange
-        //Act
-        val result = helper.isPalindrome("level")
-        //Assert
-        assertEquals(true, result)
+    fun validatePassword_InputEmptyString_expected_emptyString() {
+        val result = helper.passwordValidation("")
+        assertEquals("String is empty", result)
+    }
+    @Test
+    fun validatePassword_InputSmallString_expected_SmallStringError() {
+        val result = helper.passwordValidation("sddn")
+        assertEquals("length is less than 6", result)
+    }
+
+    @Test
+    fun validatePassword_InputLargeString_expected_LargeStringError() {
+        val result = helper.passwordValidation("sdddsdkskdsdskdskdskdkskd")
+        assertEquals("length is more than 15", result)
+    }
+
+    @Test
+    fun validatePassword_InputLargeString_expected_ValidString() {
+        val result = helper.passwordValidation("sdddsdkskdsd")
+        assertEquals("String is valid", result)
+    }
+
+    @Test
+    fun validReverseString_InputEmptyString_expected_emptyString() {
+        val result = helper.reverseString("")
+        assertEquals("", result)
+    }
+    @Test
+    fun validReverseString_InputSingleCharacter_expected_SingleCharacter() {
+        val result = helper.reverseString("c")
+        assertEquals("c", result)
+    }
+    @Test
+    fun validReverseString_InputString_expected_ReverseString() {
+        val result = helper.reverseString("abc")
+        assertEquals("cba", result)
     }
 }
